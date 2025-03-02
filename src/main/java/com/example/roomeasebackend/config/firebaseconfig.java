7b0315b8b357ca57dea2aa0a3fda77f4d0648c93
Aaryan <aaryanupadhyay68@gmail.com>
2025-03-02 22:58:04 +0530

    Docker Testing

--- src/main/java/com/example/roomeasebackend/config/firebaseconfig.java
+++ src/main/java/com/example/roomeasebackend/config/firebaseconfig.java
@@ -5,19 +5,19 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class firebaseconfig {
-
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+        ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
                 .build();
 
         return FirebaseApp.initializeApp(options);
